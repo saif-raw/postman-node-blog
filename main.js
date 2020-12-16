a6ad95ab7042d227e0325e756da3997add77e6e9
saif-raw <saif.ansari@raweng.com>
2020-12-16 17:44:10 +0530

    commit

--- main.js
+++ main.js
@@ -2,33 +2,45 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-// instantiating the express
+
 const app = express();
 
-// middleware
+
 app.use(express.json());
 
 const PORT = 3000;
 const USERS = path.join(__dirname, "data", "blogs.json");
 const blogData = JSON.parse(fs.readFileSync(USERS, "utf-8"));
 
-// get all the blogs
-app.get("/blogs", (req, res) => {
-    let data = blogData.filter((blog) => {
-        return Object.keys(req.query).every((key) => {
-            return blog[key].trim().replace(" ", "") == req.query[key].trim().replace(" ", "");
-        });
+
+
+
+    app.get("/blogs", (req, res) => {
+        if (req.query) {
+            let data = blogData.filter((blog) => {
+                return Object.keys(req.query).every((key) => {
+                    return (
+                        blog[key].trim().replace(/[#-\s]/g, "").toLowerCase() === req.query[key].trim().replace(/[#-\s]/g, "").toLowerCase()
+                    );
+                });
+            });
+            if (data < 1) {
+                res.status(404).json({
+                    status: "Blog does not exist",
+                });
+            } else {
+                res.status(200).json({
+                    status: "Success",
+                    data,
+                });
+
+            }
+        } else {
+            res.status(200).json(blogData);
+        }
     });
 
-    res.status(200).json({
-        status: "Successful",
-        data,
-    });
 
-    res.status(200).json(blogData);
-});
-
-// get the blogs passed with the id
 app.get("/blogs/:id", (req, res) => {
     let blog = blogData.find((blog) => {
         return blog.id === req.params.id;
@@ -36,16 +48,16 @@ app.get("/blogs/:id", (req, res) => {
 
     if (blog) {
         res.status(200).json({
-            status: "Successful",
+            status: "Success",
             data: blog,
         });
     } else {
         res.status(200).json({
-            status: "Blog not found",
+            status: "Blog does not exist",
         });
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on PORT : ${PORT}`);
+    console.log(`Server is on PORT : ${PORT}`);
 });
